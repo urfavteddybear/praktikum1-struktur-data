ea3c6ec4b894c6e608c1fca5378aa3a044ddf64b
unknown <business.urfavteddybear@gmail.com>
2024-04-16 07:59:29 +0800

    fix: fixing unlined array

--- aksesArray3d.py
+++ aksesArray3d.py
@@ -1,6 +1,5 @@
 import numpy as np
-arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11,
-12]]])
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
 print(arr[1, 0, 2])
 
 # perulangan array 3d
